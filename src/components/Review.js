da84f306edfdeca92e1ca215aaa34ec322aa4f17
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 22:25:45 +0530

    reviews showed on UI

---
+++ src/components/Review.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Review = ({reviews, setReviews}) => {
+  return (
+    <main>
+            {reviews.map((review, index) => (
+                <div className='review'>
+                    <img className='person-img' src={review.image} alt={review.name} />
+                    <p className='author' id={`auther-${review.id}`}>Name: {review.name}</p>
+                    <p className='job'>Job Role: {review.job}</p>
+                    <p className='info'>{review.text}</p>
+                    <div className='buttons'>
+                        <button className='prev-btn'>Previous</button>
+                        <button className='next-btn'>Next</button>
+                        <button className='rendom-btn'>surprise me</button>
+                    </div>
+                </div>
+            ))}
+    </main>
+  )
+}
+
+export default Review
