7350ec9f13318ff16badef8d6f6f036cf212f100
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 22:51:49 +0530

    Button logic added

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Review = ({reviews, setReviews}) => {
+const Review = ({ reviews, setReviews }) => {
+    const [index, setIndex] = useState(0);
+    const review = reviews[index];
+
+    const nextIndex = () => {
+        setIndex((index+1)%reviews.length)
+    }
+
+    const prevIndex = () => {
+        setIndex((index - 1 + reviews.length) % reviews.length)
+    }
+
+    const randomIndex = () => {
+        setIndex(Math.floor(Math.random() * reviews.length))
+    }
   return (
     <main>
-            {reviews.map((review, index) => (
-                <div className='review'>
-                    <img className='person-img' src={review.image} alt={review.name} />
-                    <p className='author' id={`author-${review.id}`}>Name: {review.name}</p>
-                    <p className='job'>Job Role: {review.job}</p>
-                    <p className='info'>{review.text}</p>
-                    <div className='buttons'>
-                        <button className='prev-btn'>Previous</button>
-                        <button className='next-btn'>Next</button>
-                        <button className='random-btn'>surprise me</button>
-                    </div>
-                </div>
-            ))}
+      <div className="review">
+        <img className="person-img" src={review.image} alt={review.name} />
+        <p className="author" id={`author-${review.id}`}>
+          Name: {review.name}
+        </p>
+        <p className="job">Job Role: {review.job}</p>
+        <p className="info">{review.text}</p>
+        <div className="buttons">
+          <button onClick={prevIndex} className="prev-btn">Previous</button>
+          <button onClick={nextIndex} className="next-btn">Next</button>
+          <button onClick={randomIndex} className="random-btn">surprise me</button>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
