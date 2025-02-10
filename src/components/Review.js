a7850295e73f4ae7c852bac5fc0698949c15be09
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 23:02:21 +0530

    random button changed

--- src/components/Review.js
+++ src/components/Review.js
@@ -12,17 +12,23 @@ const Review = ({ reviews, setReviews }) => {
         setIndex((index - 1 + reviews.length) % reviews.length)
     }
 
+     // Handle random review (excluding current index)
     const randomIndex = () => {
-        setIndex(Math.floor(Math.random() * reviews.length))
-    }
+        let newIndex;
+        do {
+          newIndex = Math.floor(Math.random() * reviews.length);
+        } while (newIndex === index);
+        setIndex(newIndex);
+      };
+
   return (
     <main>
       <div className="review">
         <img className="person-img" src={review.image} alt={review.name} />
-        <p className="author" id={`author-${review.id}.author`}>
-          Name: {review.name}
+        <p className="author" id={`author-${review.id}`}>
+          {review.name}
         </p>
-        <p className="job">Job Role: {review.job}</p>
+        <p className="job">{review.job}</p>
         <p className="info">{review.text}</p>
         <div className="buttons">
           <button onClick={prevIndex} className="prev-btn">Previous</button>
